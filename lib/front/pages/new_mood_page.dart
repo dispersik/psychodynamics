b34ef931ba88d0c46b421d1d3970872de3ebe723
dispersik <korotkmax@gmail.com>
2021-03-17 01:50:07 +0300

    stage

--- lib/front/pages/new_mood_page.dart
+++ lib/front/pages/new_mood_page.dart
@@ -4,6 +4,7 @@ import 'package:psychodynamics/front/ui_helper.dart';
 
 class NewMoodPage extends StatefulWidget {
   static const routeName = '/newMoodPage';
+
   @override
   _NewMoodPageState createState() => _NewMoodPageState();
 }
@@ -12,25 +13,41 @@ class _NewMoodPageState extends State<NewMoodPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
+      backgroundColor: Theme
+          .of(context)
+          .backgroundColor,
       body: SafeArea(
         child: SizedBox.expand(
           child: Column(
             children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Text('New mood record', style: TextStyle(
+                    fontSize: 20, fontWeight: FontWeight.w700, color: Theme
+                    .of(context)
+                    .primaryColor),),
+              ),
               ExpansionTile(
-                title: Text('Influencers'),
+                title: Text('Influencers', style: TextStyle(
+                    fontSize: 20, fontWeight: FontWeight.w700, color: Theme
+                    .of(context)
+                    .primaryColor),),
                 children: [
                   Text('Some children'),
                   Text('Some children'),
                 ],
               ),
               ExpansionTile(
-                title: Text('Emotions'),
+                title: Text('Emotions', style: TextStyle(
+                    fontSize: 20, fontWeight: FontWeight.w700, color: Theme
+                    .of(context)
+                    .primaryColor),),
                 children: [
                   Text('emotions'),
                 ],
               ),
-              ElevatedButton(onPressed: ()=>print("save button tap"), child: Text('Save button'))
+              ElevatedButton(onPressed: () => print("save button tap"),
+                  child: Text('Save mood record'))
             ],
           ),
         ),
